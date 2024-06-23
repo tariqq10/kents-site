17f362ea4bccb0ef6246c38c661388f145e2798f
KENT <kwilliam866@gmail.com>
2024-06-23 23:47:25 +0300

    new commit

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+function validateForm() {
+    let name = document.getElementById('name').value;
+    let phone = document.getElementById('Phone').value;
+    let review = document.getElementById('review').value;
+
+    if (name.trim() === "") {
+        alert("Name must be filled out");
+        return false;
+    }
+    
+    if (phone.trim() === "") {
+        alert("Phone must be filled out");
+        return false;
+    }
+
+    if (review.trim() === "") {
+        alert("Review must be filled out");
+        return false;
+    }
+
+    alert("Form Submitted!");
+    return true; 
+}
